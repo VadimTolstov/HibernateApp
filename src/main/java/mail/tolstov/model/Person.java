91c0f887ce335424f06b7d8cd6b33057bae5cc7b
VadimTolstov <tolstov50@mail.ru>
2024-08-25 17:50:06 +0300

    lesson_2

--- src/main/java/mail/tolstov/model/Person.java
+++ src/main/java/mail/tolstov/model/Person.java
@@ -1,9 +1,6 @@
 package mail.tolstov.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "person")
@@ -11,6 +8,7 @@ public class Person {
 
     @Id
     @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     @Column(name = "name")
     private String name;
