84c0ee87bd9c086e931f1b0280d4abd3d7aadc6c
VadimTolstov <tolstov50@mail.ru>
2024-08-25 16:34:24 +0300

    lesson_1

--- src/main/java/mail/tolstov/App.java
+++ src/main/java/mail/tolstov/App.java
@@ -1,13 +1,23 @@
 package mail.tolstov;
 
+
+import mail.tolstov.model.Person;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        Configuration configuration = new Configuration().addAnnotatedClass(Person.class);
+
+        try (SessionFactory sessionfactory = configuration.buildSessionFactory()) {
+            Session session = sessionfactory.getCurrentSession();
+            session.beginTransaction();
+            Person person = session.get(Person.class, 1);
+            System.out.println(person.getName() + " " + person.getAge());
+        }
     }
 }
